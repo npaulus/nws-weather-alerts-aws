7f32d29ae44dac08d9d723763ee3679356d61241
npaulus <nate@natepaulus.com>
2017-01-18 08:27:49 -0600

    consolidating all lambdas and website into one repo

---
+++ weather-alert-details-lambda/weather-alert-details-lambda.js
@@ -0,0 +1,45 @@
+var http = require('https');
+var parseString = require('xml2js').parseString;
+const url = require('url');
+
+exports.handler = (event, context) => {
+    
+    console.log("URL IS: " + event.params.querystring.url)
+    var weatherDetailUrl = url.parse(event.params.querystring.url);
+	var options = {
+        host : weatherDetailUrl.hostname, 
+        port : 443,
+        path : weatherDetailUrl.path, // the rest of the url with parameters if needed
+        method : 'GET' // do GET
+    };
+    console.log("Here is the path: " +options.path);
+  
+    var req = http.request(options, function(res) {
+    res.setEncoding('utf-8');
+    console.log("Status code: " + res.statusCode);
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+        
+    res.on('end', function() {
+      
+    parseString(responseString, function (err, result) {
+        var alertDetails = new Object();
+        alertDetails.headline = result.alert.info[0].headline;
+        alertDetails.polygon = result.alert.info[0].area[0].polygon;
+        alertDetails.description = result.alert.info[0].description;
+        alertDetails.instructions = result.alert.info[0].instruction;
+    console.log(JSON.stringify(result));
+    context.succeed(alertDetails);
+      });
+    });
+    
+    req.on('error', function (e) {
+      console.log(e);
+    });
+  });
+
+  req.end();
+}
---
+++ weather-alerts-lambda/weather-alerts-lambda.js
@@ -0,0 +1,46 @@
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
+exports.handler = (event, context) => {
+	console.log("FIPS CODE IS: " + event.params.querystring.fips)
+	var options = {
+        host : 'alerts.weather.gov', 
+        port : 443,
+        path : '/cap/wwaatmget.php?x='+event.params.querystring.fips+'&y=0', // the rest of the url with parameters if needed
+        method : 'GET' // do GET
+    };
+    console.log("Here is the path: " +options.path);
+  
+    var req = http.request(options, function(res) {
+    res.setEncoding('utf-8');
+    console.log("Status code: " + res.statusCode);
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+
+    res.on('end', function() {
+       parseString(responseString, function (err, jsonResult) {
+          console.log(JSON.stringify(jsonResult));
+          var result = new Object();
+          result.alerts = [];
+          for(var index = 0; index < jsonResult.feed.entry.length; index++){
+            result.alerts[index] = new Object();
+            result.alerts[index].url = jsonResult.feed.entry[index].id;
+            result.alerts[index].title = jsonResult.feed.entry[index].title;
+            result.alerts[index].summary = jsonResult.feed.entry[index].summary;
+            result.alerts[index].coordinates = jsonResult.feed.entry[index]['cap:polygon'];
+          }
+
+          context.succeed(result);
+       });
+    });
+    
+    req.on('error', function (e) {
+      console.log(e);
+    });
+  });
+
+  req.end();
+};
