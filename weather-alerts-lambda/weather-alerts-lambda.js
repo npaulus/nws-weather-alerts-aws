c518f159e0e8bf4f89987f026f674c85cf431699
Nate Paulus <Nate.Paulus@icfi.com>
2017-02-03 15:01:42 -0600

    Updated to use node-fetch instead of https for weather-alerts-lambda

--- weather-alerts-lambda/weather-alerts-lambda.js
+++ weather-alerts-lambda/weather-alerts-lambda.js
@@ -1,47 +1,35 @@
 var http = require('https');
+var fetch = require('node-fetch');
 var parseString = require('xml2js').parseString;
 
 exports.handler = (event, context) => {
 	console.log("FIPS CODE IS: " + event.params.querystring.fips)
-	var options = {
-        host : 'alerts.weather.gov', 
-        port : 443,
-        path : '/cap/wwaatmget.php?x='+event.params.querystring.fips+'&y=0', // the rest of the url with parameters if needed
-        method : 'GET' // do GET
-    };
-    console.log("Here is the path: " +options.path);
-  
-    var req = http.request(options, function(res) {
-    res.setEncoding('utf-8');
-    console.log("Status code: " + res.statusCode);
-    var responseString = '';
-
-    res.on('data', function(data) {
-      responseString += data;
-    });
-
-    res.on('end', function() {
-       parseString(responseString, function (err, jsonResult) {
-          console.log(JSON.stringify(jsonResult));
-          var result = new Object();
-          result.alerts = [];
-          for(var index = 0; index < jsonResult.feed.entry.length; index++){
-            result.alerts[index] = new Object();
-            result.alerts[index].feedTitle = jsonResult.feed.title;
-            result.alerts[index].url = jsonResult.feed.entry[index].id;
-            result.alerts[index].title = jsonResult.feed.entry[index].title;
-            result.alerts[index].summary = jsonResult.feed.entry[index].summary;
-            result.alerts[index].coordinates = jsonResult.feed.entry[index]['cap:polygon'];
-          }
-
-          context.succeed(result);
-       });
-    });
     
-    req.on('error', function (e) {
-      console.log(e);
-    });
-  });
-
-  req.end();
+    fetch('https://alerts.weather.gov' + '/cap/wwaatmget.php?x='+event.params.querystring.fips+'&y=0')
+        .then(function(res){
+            console.log("Status is: " + res.status);
+            if(res.ok){
+                return res.text();
+            } else {
+                throw Error(res.statusText)
+            }
+        }).then(function(text){
+            parseString(text, function (err, jsonResult) {
+              console.log(JSON.stringify(jsonResult));
+              var result = new Object();
+              result.alerts = [];
+              for(var index = 0; index < jsonResult.feed.entry.length; index++){
+                result.alerts[index] = new Object();
+                result.alerts[index].feedTitle = jsonResult.feed.title;
+                result.alerts[index].url = jsonResult.feed.entry[index].id;
+                result.alerts[index].title = jsonResult.feed.entry[index].title;
+                result.alerts[index].summary = jsonResult.feed.entry[index].summary;
+                result.alerts[index].coordinates = jsonResult.feed.entry[index]['cap:polygon'];
+              }
+              context.succeed(result);
+            });
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 };
